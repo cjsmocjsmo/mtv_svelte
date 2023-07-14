238f061cd7c3c4cf7303fd3ef28b2299fdfee1d1
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-07-14 06:32:00 -0700

    finished movies

--- src/routes/starwars/+page.js
+++ src/routes/starwars/+page.js
@@ -1,6 +1,6 @@
 export async function load({ fetch }) {
     
-        const res = await fetch('http://192.168.0.26:8080/kingsman');
+        const res = await fetch('http://192.168.0.26:8080/starwars');
         const movlist = await res.json();
    
     return { movlist }
