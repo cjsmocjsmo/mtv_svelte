9b188b115d65c59cdb16569ff979aa02de75b3f2
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-07-16 13:25:49 -0700

    added star trek

---
+++ src/routes/tvSeasonsVisions/+page.js
@@ -0,0 +1,7 @@
+// export async function load({ fetch }) {
+    
+//     const res = await fetch('http://192.168.0.26:8080/starwars/visions/01');
+//     const season1 = await res.json();
+
+// return { season1 }
+// }
---
+++ src/routes/tvstarwars/+page.svelte
@@ -0,0 +1,61 @@
+<script>
+	import BackArrow from '../../components/BackArrow.svelte';
+	import TvAndorCard from '../../components/TVAndorCard.svelte';
+	import AndorImg from '$lib/images/starwars/andor.avif';
+	import TvBadBatchCard from '../../components/TVBadBatchCard.svelte';
+	import BadBatchImg from '$lib/images/starwars/badbatch.avif';
+	import TvBobbaFettCard from '../../components/TVBobbaFettCard.svelte';
+	import BobbaFettImg from '$lib/images/starwars/bobafett.avif';
+	import TvMandalorianCard from '../../components/TvMandalorianCard.svelte';
+	import MandalorianImg from '$lib/images/starwars/mandalorian.avif';
+	import TvTalesOfTheJediCard from '../../components/TVTalesOfTheJediCard.svelte';
+	import TalesOfTheJediImg from '$lib/images/starwars/talesofthejedi.avif';
+	import TvObiWanCard from '../../components/TVObiWanCard.svelte';
+	import ObiWanImg from '$lib/images/starwars/obiwan.avif';
+	import TvVisionsCard from '../../components/TVVisionsCard.svelte';
+	import VisionsImg from '$lib/images/starwars/visions.avif';
+</script>
+
+<div>
+	<BackArrow path="/tvshows" />
+	<h1>Star Trek</h1>
+	<div class="foo">
+		<div class="bar">
+			<TvAndorCard src={AndorImg} />
+			<TvBadBatchCard src={BadBatchImg} />
+			<TvBobbaFettCard src={BobbaFettImg} />
+			<TvMandalorianCard src={MandalorianImg} />
+			<TvTalesOfTheJediCard src={TalesOfTheJediImg} />
+			<TvObiWanCard src={ObiWanImg} />
+			<TvVisionsCard src={VisionsImg} />
+		</div>
+	</div>
+</div>
+
+<style>
+	.bar {
+		display: grid;
+		grid-template-columns: repeat(1, 1fr);
+		grid-gap: 1rem;
+	}
+
+	h1 {
+		color: yellowgreen;
+	}
+
+	@media (min-width: 500px) {
+		.bar {
+			grid-template-columns: repeat(2, 1fr);
+		}
+	}
+	@media (min-width: 700px) {
+		.bar {
+			grid-template-columns: repeat(3, 1fr);
+		}
+	}
+	@media (min-width: 900px) {
+		.bar {
+			grid-template-columns: repeat(4, 1fr);
+		}
+	}
+</style>
