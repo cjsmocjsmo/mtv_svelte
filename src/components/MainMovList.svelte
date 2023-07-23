d98306429663ddf5185b8fcdbb0a17db51fa7b03
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-07-22 22:13:51 -0700

    refactor

---
+++ src/components/MainMovList.svelte
@@ -0,0 +1,57 @@
+
+
+<div class="movlist">
+	<a data-sveltekit-preload-data="tap" href="action"><p class="mov">Action</p></a>
+	<a data-sveltekit-preload-data="tap" href="arnold"><p class="mov">Arnold</p></a>
+	<a data-sveltekit-preload-data="tap" href="brucelee"><p class="mov">Bruce Lee</p></a>
+	<a data-sveltekit-preload-data="tap" href="brucewillis"><p class="mov">Bruce Willis</p></a>
+	<a data-sveltekit-preload-data="tap" href="buzz"><p class="mov">Buzz</p></a>
+	<a data-sveltekit-preload-data="tap" href="cartoons"><p class="mov">Cartoons</p></a>
+	<a data-sveltekit-preload-data="tap" href="charliebrown"><p class="mov">Charlie Brown</p></a>
+	<a data-sveltekit-preload-data="tap" href="chucknorris"><p class="mov">Chuck Norris</p></a>
+	<a data-sveltekit-preload-data="tap" href="comedy"><p class="mov">Comedy</p></a>
+	<a data-sveltekit-preload-data="tap" href="drama"><p class="mov">Drama</p></a>
+	<a data-sveltekit-preload-data="tap" href="documentary"><p class="mov">Documentary</p></a>
+	<a data-sveltekit-preload-data="tap" href="eternalquon"><p class="mov">Eternal Quon</p></a>
+	<a data-sveltekit-preload-data="tap" href="fantasy"><p class="mov">Fantasy</p></a>
+	<a data-sveltekit-preload-data="tap" href="godzilla"><p class="mov">Godzilla</p></a>
+	<a data-sveltekit-preload-data="tap" href="harrypotter"><p class="mov">Harry Potter</p></a>
+	<a data-sveltekit-preload-data="tap" href="indianajones"><p class="mov">Indiana Jones</p></a>
+	<a data-sveltekit-preload-data="tap" href="jamesbond"><p class="mov">James Bond</p></a>
+	<a data-sveltekit-preload-data="tap" href="johnwayne"><p class="mov">John Wayne</p></a>
+	<a data-sveltekit-preload-data="tap" href="johnwick"><p class="mov">John Wick</p></a>
+	<a data-sveltekit-preload-data="tap" href="jurassicpark"><p class="mov">Jurrasic Park</p></a>
+	<a data-sveltekit-preload-data="tap" href="kingsman"><p class="mov">KingsMan</p></a>
+	<a data-sveltekit-preload-data="tap" href="meninblack"><p class="mov">Men In Black</p></a>
+	<a data-sveltekit-preload-data="tap" href="minions"><p class="mov">Minions</p></a>
+	<a data-sveltekit-preload-data="tap" href="misc"><p class="mov">Misc</p></a>
+	<a data-sveltekit-preload-data="tap" href="nicolascage"><p class="mov">Nicolas Cage</p></a>
+	<a data-sveltekit-preload-data="tap" href="oldies"><p class="mov">Oldies</p></a>
+	<a data-sveltekit-preload-data="tap" href="pirates"><p class="mov">Pirates</p></a>
+	<a data-sveltekit-preload-data="tap" href="riddick"><p class="mov">Riddick</p></a>
+	<a data-sveltekit-preload-data="tap" href="scifi"><p class="mov">SciFi</p></a>
+	<a data-sveltekit-preload-data="tap" href="startrek"><p class="mov">Star Trek</p></a>
+	<a data-sveltekit-preload-data="tap" href="starwars"><p class="mov">Star Wars</p></a>
+	<a data-sveltekit-preload-data="tap" href="superheros"><p class="mov">Super Heros</p></a>
+	<a data-sveltekit-preload-data="tap" href="tinkerbell"><p class="mov">Tinker Bell</p></a>
+	<a data-sveltekit-preload-data="tap" href="tomcruize"><p class="mov">Tom Cruize</p></a>
+	<a data-sveltekit-preload-data="tap" href="transformers"><p class="mov">Transformers</p></a>
+	<a data-sveltekit-preload-data="tap" href="tremors"><p class="mov">Tremors</p></a>
+	<a data-sveltekit-preload-data="tap" href="therock"><p class="mov">The Rock</p></a>
+	<a data-sveltekit-preload-data="tap" href="xmen"><p class="mov">XMen</p></a>
+</div>
+
+<style>
+	.movlist {
+		display: grid;
+		grid-template-columns: repeat(3, 1fr);
+		grid-template-rows: repeat(10, 1fr);
+		column-gap: 4em;
+		row-gap: 0.25em;
+	}
+	p {
+		font-size: 1.75em;
+		justify-self: center;
+		align-self: center;
+	}
+</style>
