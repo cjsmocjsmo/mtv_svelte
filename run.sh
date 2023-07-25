b520c73be5ab2a5c86d0b8287314c167cafb5f0d
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-07-25 01:05:50 -0700

    update

--- run.sh
+++ run.sh
@@ -10,3 +10,5 @@ npm i -D @sveltejs/adapter-node;
 npm install;
 npm run build;
 PORT=3000 nohup node build >/dev/null 2>&1 &
+
+# ORIGIN="http://192.168.0.74:3000" PORT=3000 node -r dotenv/config build
--- service.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd /home/pi/mtv_svelte;
-node build
-# ORIGIN="http://192.168.0.74:3000" PORT=3000 node -r dotenv/config build
