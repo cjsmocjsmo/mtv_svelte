ade8837f7f66d75a42a97dd054da88e2fc39c052
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-07-24 21:45:07 -0700

    update for prod

--- run.sh
+++ run.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
 git pull;
-
-
-
 npm i -D @sveltejs/adapter-node;
 npm install dotenv;
 npm install;
-ORIGIN="http://192.168.0.26:3000" PORT=3000 node -r dotenv/config build
+ORIGIN="http://192.168.0.74:3000" PORT=3000 node -r dotenv/config build
