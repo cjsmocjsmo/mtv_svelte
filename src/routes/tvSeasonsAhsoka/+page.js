f740b704417c412aa97a16c50d381552d86e4afb
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-08-23 22:08:17 -0700

    update

--- src/routes/tvSeasonsAhsoka/+page.js
+++ src/routes/tvSeasonsAhsoka/+page.js
@@ -1,7 +1,7 @@
 
 export async function load({ fetch }) {
 
-    let addr = 'http://192.168.0.94:8080//starwars/ahsoka/01'
+    let addr = 'http://192.168.0.94:8080/starwars/ahsoka/01'
 
     const res = await fetch(addr);
     const season1 = await res.json();
